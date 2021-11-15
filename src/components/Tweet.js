5892090dada7e0123295c90db613228b11f5c1d3
Sylvia <snacgyver@gmail.com>
2021-11-15 14:42:50 -0500

    completed lab

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,49 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
+function Tweet(props) {
+  console.log(props)
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
+      {/* <img
+        src={props.tweet.user.image}
         className="profile"
         alt="profile"
-      />
+      /> */
+      
+      <ProfileImage image={props.tweet.user.image}/>
+      }
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          {/* <span className="user">
+            <span className="name">{props.tweet.user.name}</span>
+            <span className="handle">@{props.tweet.user.handle}</span>
+          </span> */}
+          <User userData={props.tweet.user}/>
 
-          <span className="timestamp">Nov 30, 2020</span>
+
+          {/* <span className="timestamp">{props.tweet.timestamp}</span> */}
+          <Timestamp time={props.tweet.timestamp}/>
+
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        {/* <p className="message">
+          {props.tweet.message}
+        </p> */}
+        <Message message={props.tweet.message}/>
 
-        <div className="actions">
           {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        {/* <div className="actions">
+          <i className="far fa-comment"></i>
+          <i className="fas fa-retweet"></i>
+          <i className="far fa-heart"></i>
+          <i className="fas fa-share"></i>
+        </div> */}
+        <Actions/>
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
